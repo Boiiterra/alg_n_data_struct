90c34bd3c4d7c8187f798dafbbc160341d0f9429
Boiiterra <81579070+Boiiterra@users.noreply.github.com>
2024-08-06 23:41:39 +0300

    Create array with random stuff in it.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         HelloWorld.say();
+
+        int maxLen = 30, minLen = 10; // Array data
+        int maxInt = 100, minInt = -100; // Array int
+
+        Random rng = new Random();
+
+        if (maxLen < minLen) {
+            throw new RuntimeException("Minimum array length shouldn't be bigger than maximum.");
+        } else if (maxLen <= 1 || minLen <= 1) {
+            throw new RuntimeException("Array length should be more than 1. What will you sort? Minus one numba? WHY?!");
+        }
+
+        if (maxInt <= minInt) {
+            throw new RuntimeException("Why do you want to sort the same NUMBA?");
+        }
+
+        int arr_size = rng.nextInt(maxLen - minLen + 1) + minLen;
+        int[] arr = new int[arr_size];
+
+        for (int i = 0; i < arr_size; i++) {
+            arr[i] = rng.nextInt(1 + maxInt - minInt) + minInt;
+        }
+
+        System.out.println(Arrays.toString(arr));
+
     }
 }
